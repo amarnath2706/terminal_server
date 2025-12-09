f6b5e12f58af9732c40af1263589324623aedf2d
amarnath2706 <amarnath.arjsd@gmail.com>
2025-12-09 15:41:36 +0530

    first commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from mcp.server.fastmcp import FastMCP
 #initial the name of the MCP
 mcp = FastMCP("terminal")
 
-DEFAULT_WORKSPACE = os.path.expanduser("~/MCP/servers/workspace")
+DEFAULT_WORKSPACE = os.path.expanduser("~/MCP/workspace")
 
 @mcp.tool()
 async def run_command(command: str) -> str:
